1b5c26085e68cdcfc5520fa91fdfd5dcdaf20e23
Stephen Herr <sherr@redhat.com>
2018-09-04 19:25:13 -0400

    Initial commit on what a data model and OpenControl import script might look like. Incomplete.
    Note also that this depends on 'python manage.py migrate' being
    run on the openshift container to run the sql commands, which I
    don't know how to make happen yet.

--- scorecard/models.py
+++ scorecard/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+
+# OpenControl data
+class Standard(models.Model):
+    name = models.CharField(max_length=50)
+    label = models.CharField(max_length=10)
+
+
+class ControlFamily(models.Model):
+    label = models.CharField(max_length=10)
+
+
+class Control(models.Model):
+    name = models.TextField()
+    label = models.CharField(max_length=10)
+    family = models.ForeignKey(ControlFamily, on_delete=models.CASCADE)
+    standard = models.ForeignKey(Standard, on_delete=models.CASCADE)
+
+
+class ProductControl(models.Model):
+    status = models.CharField(max_length=20)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    control = models.ForeignKey(Control, on_delete=models.CASCADE)
+
