d01093dddecf84b3ec8d73312005d26a7428d4c7
Jonathan Garrison <jgarriso@redhat.com>
2018-09-19 15:07:09 -0400

    Added default Django admin site and turned on debugging.

--- scorecard/admin.py
+++ scorecard/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Control, ControlFamily, Product, ProductControl, Standard
 
-# Register your models here.
+
+class ControlAdmin(admin.ModelAdmin):
+    list_display = ('name', 'label', 'family', 'standard')
+
+
+class ControlFamilyAdmin(admin.ModelAdmin):
+    list_display = ('label',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ProductControlAdmin(admin.ModelAdmin):
+    list_display = ('status', 'product', 'control')
+
+
+class StandardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'label')
+
+
+admin.site.register(Control, ControlAdmin)
+admin.site.register(ControlFamily, ControlFamilyAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductControl, ProductControlAdmin)
+admin.site.register(Standard)
